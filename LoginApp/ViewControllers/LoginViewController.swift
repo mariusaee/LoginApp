99e302d5a0d8ba5b6239e9c4f8a2a30284cc389c
mariusaee <mariusae@me.com>
2021-02-07 17:24:22 +0300

    MVC started

--- LoginApp/ViewControllers/LoginViewController.swift
+++ LoginApp/ViewControllers/LoginViewController.swift
@@ -78,15 +78,3 @@ extension LoginViewController: UITextFieldDelegate {
     }
 }
 
-// MARK: - Set background color
-extension UIView {
-    func addVerticalGradientLayer(topColor: UIColor, bottomColor: UIColor) {
-        let gradient = CAGradientLayer()
-        gradient.frame = bounds
-        gradient.colors = [topColor.cgColor, bottomColor.cgColor]
-        gradient.locations = [0.0, 1.0]
-        gradient.startPoint = CGPoint(x: 0, y: 0)
-        gradient.endPoint = CGPoint(x: 0, y: 1)
-        layer.insertSublayer(gradient, at: 0)
-    }
-}
